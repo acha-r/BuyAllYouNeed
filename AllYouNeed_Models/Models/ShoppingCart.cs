6cdc890cdfdcb078a851bd191b434ecb7bdda702
Rachel Okorie <jayrachel96@gmail.com>
2023-06-28 13:19:14 +0100

    Created shoppingcart collection

---
+++ AllYouNeed_Models/Models/ShoppingCart.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDbGenericRepository.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllYouNeed_Models.Models
+{
+    [CollectionName("Cart")]
+    public class ShoppingCart
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId Id { get; set; }
+        public Dictionary<string, int>? Products { get; set; }
+
+        [EmailAddress]
+        public string BuyerEmail { get; set; } = string.Empty;
+        public string DeliveryAddress { get; set; } = string.Empty;
+        public bool HasPaid { get; set; } = false;
+    }
+}
