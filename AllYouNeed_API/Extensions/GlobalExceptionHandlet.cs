4764fa7daa23a5244b3e4c28e1fbe486cb6b941a
Rachel Okorie <jayrachel96@gmail.com>
2023-07-02 03:48:32 +0100

    Add global exception handler

---
+++ AllYouNeed_API/Extensions/GlobalExceptionHandlet.cs
@@ -0,0 +1,60 @@
+﻿using AllYouNeed_Models.DTOS.Respoonses;
+using Microsoft.AspNetCore.Diagnostics;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace AllYouNeed_API.Extensions
+{
+    public static class ExceptionHandler
+    {
+        public static void ConfigureException(this IApplicationBuilder app, IWebHostEnvironment hostEnvironment)
+        {
+
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.ContentType = "application/json";
+
+                    IExceptionHandlerFeature? exceptionHandleFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    if (exceptionHandleFeature != null)
+                    {
+                        var status = 500;
+                        switch (exceptionHandleFeature.Error)
+                        {
+
+
+                            case InvalidOperationException:
+                            case InvalidDataException:
+                            case KeyNotFoundException:
+                            case ArgumentException:
+
+                                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                                status = 400;
+                                break;
+                            case UnauthorizedAccessException:
+                                context.Response.StatusCode = StatusCodes.Status403Forbidden;
+                                status = 403;
+                                break;
+                            default:
+                                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                                break;
+                        }
+
+                        ErrorResponse err = new() { Success = false, Status = status };
+                        err.Message =
+                            hostEnvironment.IsProduction() && context.Response.StatusCode ==
+                            StatusCodes.Status500InternalServerError
+                                ? "We currently cannot complete this request process. Please retry or contact our support"
+                                : exceptionHandleFeature.Error.Message;
+
+                        var serializerSettings = new JsonSerializerSettings();
+                        serializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                        string msg = JsonConvert.SerializeObject(err, serializerSettings);
+                        await context.Response.WriteAsync(msg);
+                    }
+                });
+            });
+        }
+    }
+}
---
+++ AllYouNeed_Models/DTOS/Respoonses/ErrorResponse.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace AllYouNeed_Models.DTOS.Respoonses
+{
+    public class ErrorResponse
+    {
+        public int Status { get; set; }
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
